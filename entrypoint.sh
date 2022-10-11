c9e8845b8be3f6cb5f2705e50504ae9818d04bc5
Marcelo Juchem <juchem@gmail.com>
2022-10-10 21:44:14 -0500

    entrypoint: calling `install` with `--force` by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -e
 
+build_version="${VERSION:-HEAD}"
+
 build_alacritty() {
   apt-get update
   apt-get install --only-upgrade -y --no-install-recommends \
     cargo rustc
 
-  git fetch --depth=1 origin "${VERSION}"
-  git checkout -b "build-${VERSION}" FETCH_HEAD
+  git fetch --depth=1 origin "${build_version}"
+  git checkout -b "build-${build_version}" FETCH_HEAD
   git clean -xfd
 
   time cargo build --release
-  time cargo install alacritty --root /out "$@"
+  time cargo install alacritty --root /out --force "$@"
 }
 
 (set -x; build_alacritty "$@")
 
 cat <<EOF
 
-Alacritty ${VERSION} successfully built.
+Alacritty ${build_version} successfully built.
 
 Customize it by editing '~/.alacritty.yml'.
 
-A sample default config can be downloaded from 'https://raw.githubusercontent.com/alacritty/alacritty/${VERSION}/alacritty.yml'.
+A sample default config can be downloaded from 'https://raw.githubusercontent.com/alacritty/alacritty/${build_version}/alacritty.yml'.
 EOF
